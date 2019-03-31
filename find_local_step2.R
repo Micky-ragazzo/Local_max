364ff959c26d7ace6ae76ae2672bad0135bb495c
Micky-ragazzo <w48778@student.wsiz.rzeszow.pl>
2019-03-31 21:54:57 +0200

    message

---
+++ find_local_step2.R
@@ -0,0 +1,11 @@
+functionMAX_MIN <- function(x,y)
+{
+      if(nrow(data.frame(peakdet(x, .5, z)) > 1))
+       {
+         return(print("NOK"))
+       } else
+         {
+         return(print("OK"))
+       }
+        
+}
